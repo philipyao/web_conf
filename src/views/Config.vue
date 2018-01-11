bde961036b9b6976e177a0df79c3e436d9d8cb21
philipyao <187239625@qq.com>
2018-01-11 13:54:26 +0800

    update

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -8,7 +8,7 @@
 <template>
     <div>
         <div style="height: 60px; padding: 10px 30px 20px 0; display:flex; justify-content:flex-end;">
-            <Button type="success" @click="handleUpdate">
+            <Button type="success" @click="showUpdate">
                 发布修改
             </Button>
             <Button @click="handleReset">
@@ -22,34 +22,6 @@
                 <Page :total="totalCount" :current="1"></Page>
             </div>
         </div>
-<!--         <Modal v-model="modalCreate">
-            <Form  :model="formCreateMaterial">
-                <FormItem label="素材名称">
-                    <Input v-model="formCreateMaterial.name"></Input>
-                </FormItem>
-                <FormItem>
-                    <Upload
-                        ref="uploadCtl"
-                        type="select"
-                        name="file"
-                        :before-upload="beforeUpload"
-                        :format="['wav','amr','mp3', 'wma']"
-                        :max-size="2048"
-                        :on-format-error="handleFormatError"
-                        :on-exceeded-size="handleMaxSize"
-                        action="//10.1.164.45:8080/api/admin/upload">
-                        <div style="padding: 20px 0">
-                            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
-                            <p>点击或将文件拖拽到这里上传</p>
-                        </div>
-                    </Upload>                
-                </FormItem>                                     
-            </Form> 
-            <div slot="footer">
-                <Button size="large" @click="modalCreate = false">取消</Button>
-                <Button type="success" size="large" @click="handleCreateMaterial">确定</Button>
-            </div>            
-        </Modal>   -->
 
         <!--编辑 Modal 对话框-->
         <Modal v-model="editModal" title="修改配置" >
@@ -75,6 +47,32 @@
             </div>
         </Modal>
 
+        <!-- 发布对话框 -->
+        <Modal v-model="editUpdateModal" title="编辑发布信息" width="800">
+            <div>
+                <div style="margin-bottom: 24px; display: flex;">
+                    <div style="border: 1px solid black">
+                        <label>变动起来</label>                 
+                    </div>
+                    <div style="margin-left:80px; border: 1px solid black">
+                        <Table :columns="tblChanges" :data="changesData"></Table>
+                    </div>
+                </div>
+                <Form ref="editUpdate" :model="editUpdate" :label-width="80" >
+                    <Form-item label="发布标题" style="border: 1px solid black">
+                        <Input v-model="editUpdate.name" ></Input>
+                    </Form-item>
+                    <Form-item label="发布说明" :rows="4">
+                        <Input v-model="editUpdate.comment" type="textarea"></Input>
+                    </Form-item>
+                </Form>
+            </div>
+            <div slot="footer">
+                <Button type="primary" @click="editUpdateSubmit('editUpdate')">发布</Button>
+                <Button type="ghost" @click="editUpdateCancel()" style="margin-left: 8px">取消</Button>
+            </div>
+        </Modal>
+
     </div>
 </template>
 
@@ -94,6 +92,34 @@
                 editForm: {},
                 //编辑 modal
                 editModal: false,
+
+                editUpdate: {},
+                editUpdateModal: false,
+
+                changesData: [],
+
+                tblChanges: [
+                    {
+                        title: '命名空间',
+                        key: 'namespace',
+                    },                
+                    {
+                        title: '配置名',
+                        key: 'key',
+                    },
+                    {
+                        title: '发布前值',
+                        key: 'old_value',
+                    },
+                    {
+                        title: '发布后值',
+                        key: 'value',
+                    },
+                    {
+                        title: '作者',
+                        key: 'author',
+                    },    
+                ],
 
                 tblColumns: [
                     {
@@ -250,6 +276,12 @@
             editCancel() {
                 this.editModal = false;
             },
+            editUpdateSubmit(name) {
+                this.editUpdateModal = false
+            },
+            editUpdateCancel() {
+                this.editUpdateModal = false
+            },
             rowClassName (row, index) {
                 if (row.value != row.oldvalue) {
                     return 'table-change-row';
@@ -257,58 +289,60 @@
                     return '';
                 }
             },
-            handleUpdate() {
-                var updates = [];
-                for (var i = 0; i < this.tblData.length; i++) {
-                    if (this.tblData[i].value !== this.tblData[i].oldvalue) {
-                        updates.push({
-                            id: this.tblData[i].id,
-                            value: this.tblData[i].value,
-                        });
-                    }
-                }
-                console.log("updates: ", updates)
-                if (updates.length == 0) {
-                    this.$Message.error("没有修改")
-                    return
-                }
-                this.$jsonHttp.post('/api/update', {updates: updates}).then((response) => {
-                    if (response.status != 200) {
-                      this.$Message.error("失败：%d %s", response.status, response.statusText)
-                      console.error("失败：%d %s", response.status, response.statusText)
-                      return
-                    }
-                    var succ_cnt = 0;
-                    var fail_cnt = 0;
-                    console.log(response.data)
-                    if (Array.isArray(response.data.entries)) {
-                        succ_cnt = response.data.entries.length;
-                        for (var i = 0; i < succ_cnt; i++) {
-                            var entry = response.data.entries[i];
-                            for (var j = 0; j < this.tblData.length; j++) {
-                                if (this.tblData[j].id == entry.id) {
-                                    console.log("update ", entry.id);
-                                    this.tblData[j].value = entry.value;
-                                    this.tblData[j].oldvalue = this.tblData[j].value;
-                                    this.tblData[j].version = entry.version;
-                                    this.tblData[j].updated = entry.updated;
-                                    break
-                                }
-                            }
-                        }          
-                    }
-                    if (Array.isArray(response.data.errmsgs)) {
-                        fail_cnt = response.data.errmsgs.length;
-                        for (var i = 0; i < fail_cnt; i++) {
-                            var errmsg = response.data.errmsgs[i];
-                            console.log(errmsg);
-                        }
-                    }                
-                    this.$Message.info("成功: %d / %d", succ_cnt, succ_cnt + fail_cnt);
-                }).catch((error) => {
-                    this.$Message.error(error)
-                    console.log(error)
-                })                  
+            showUpdate() {
+                this.editUpdateModal = true;
+
+                // var updates = [];
+                // for (var i = 0; i < this.tblData.length; i++) {
+                //     if (this.tblData[i].value !== this.tblData[i].oldvalue) {
+                //         updates.push({
+                //             id: this.tblData[i].id,
+                //             value: this.tblData[i].value,
+                //         });
+                //     }
+                // }
+                // console.log("updates: ", updates)
+                // if (updates.length == 0) {
+                //     this.$Message.error("没有修改")
+                //     return
+                // }
+                // this.$jsonHttp.post('/api/update', {updates: updates}).then((response) => {
+                //     if (response.status != 200) {
+                //       this.$Message.error("失败：%d %s", response.status, response.statusText)
+                //       console.error("失败：%d %s", response.status, response.statusText)
+                //       return
+                //     }
+                //     var succ_cnt = 0;
+                //     var fail_cnt = 0;
+                //     console.log(response.data)
+                //     if (Array.isArray(response.data.entries)) {
+                //         succ_cnt = response.data.entries.length;
+                //         for (var i = 0; i < succ_cnt; i++) {
+                //             var entry = response.data.entries[i];
+                //             for (var j = 0; j < this.tblData.length; j++) {
+                //                 if (this.tblData[j].id == entry.id) {
+                //                     console.log("update ", entry.id);
+                //                     this.tblData[j].value = entry.value;
+                //                     this.tblData[j].oldvalue = this.tblData[j].value;
+                //                     this.tblData[j].version = entry.version;
+                //                     this.tblData[j].updated = entry.updated;
+                //                     break
+                //                 }
+                //             }
+                //         }          
+                //     }
+                //     if (Array.isArray(response.data.errmsgs)) {
+                //         fail_cnt = response.data.errmsgs.length;
+                //         for (var i = 0; i < fail_cnt; i++) {
+                //             var errmsg = response.data.errmsgs[i];
+                //             console.log(errmsg);
+                //         }
+                //     }                
+                //     this.$Message.info("成功: %d / %d", succ_cnt, succ_cnt + fail_cnt);
+                // }).catch((error) => {
+                //     this.$Message.error(error)
+                //     console.log(error)
+                // })                  
             },
             handleReset() {
                 this.$Message.info('reset!');
