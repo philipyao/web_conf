39adc6a5f379e6d7f5132715e57e765f6f8b42b1
philipyao <187239625@qq.com>
2018-01-19 20:29:41 +0800

    x

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -27,6 +27,19 @@
             </div>
         </div>
 
+        <Modal v-model="reLogin" width="360">
+            <p slot="header" style="color:#f60;text-align:center">
+                <Icon type="information-circled"></Icon>
+                <span>重新登录</span>
+            </p>
+            <div style="text-align:center">
+                <p>当前网页会话已过期，请重新登录。</p>
+            </div>
+            <div slot="footer">
+                <Button type="warning" size="large" long  @click="handleReLogin">确定</Button>
+            </div>
+        </Modal>
+
         <!--编辑 Modal 对话框-->
         <Modal v-model="editModal" title="修改配置" >
             <div>
@@ -118,6 +131,9 @@
                 audioSrc: "",
                 totalCount: 0,
                 tblData: [],
+
+                reLogin: false,
+
 
                 editIndex: -1,
                 editForm: {},
@@ -294,16 +310,11 @@
     	},
     	methods: {
             handleList() {
-                this.$jsonHttp.post('/api/list', {}).then((response) => {
-                    if (response.status != 200) {
-                      this.$Message.error("失败：%d %s", response.status, response.statusText)
-                      console.error("失败：%d %s", response.status, response.statusText)
-                      return
-                    }
-                    if (Array.isArray(response.data.entries)) {
+                this.sendRequest("/api/list", {}, (response) => {
+                    if (Array.isArray(response.entries)) {
                         this.totalCount = this.tblData.length
-                        for (var i = 0; i < response.data.entries.length; i++) {
-                            var entry = response.data.entries[i];
+                        for (var i = 0; i < response.entries.length; i++) {
+                            var entry = response.entries[i];
                             var config = {
                                 isnew: false,
                                 id: entry.id,
@@ -319,10 +330,7 @@
                             this.tblData.push(config);
                         }
                     }
-                }).catch((error) => {
-                    this.$Message.error(error)
-                    console.log(error)
-                })                  
+                });               
             },
             handleEdit(index) {
                 //打开 modal 窗口
@@ -380,19 +388,15 @@
                                 });                                
                             }
                         }
-                        this.$jsonHttp.post('/api/change', reqpkg).then((response) => {
-                            if (response.status != 200) {
-                              this.$Message.error("失败：%d %s", response.status, response.statusText)
-                              console.error("失败：%d %s", response.status, response.statusText)
-                              return
-                            }
-                            var succ_cnt = 0;
+
+                        this.sendRequest("/api/change", reqpkg, (response) => {
+                           var succ_cnt = 0;
                             var fail_cnt = 0;
-                            console.log(response.data)
-                            if (Array.isArray(response.data.entries)) {
-                                succ_cnt = response.data.entries.length;
+                            console.log(response)
+                            if (Array.isArray(response.entries)) {
+                                succ_cnt = response.entries.length;
                                 for (var i = 0; i < succ_cnt; i++) {
-                                    var entry = response.data.entries[i];
+                                    var entry = response.entries[i];
                                     for (var j = 0; j < this.tblData.length; j++) {
                                         var data = this.tblData[j]
                                         if (data.namespace == entry.namespace && data.key == entry.key) {
@@ -416,18 +420,15 @@
                                     }
                                 }          
                             }
-                            if (Array.isArray(response.data.errmsgs)) {
-                                fail_cnt = response.data.errmsgs.length;
+                            if (Array.isArray(response.errmsgs)) {
+                                fail_cnt = response.errmsgs.length;
                                 for (var i = 0; i < fail_cnt; i++) {
-                                    var errmsg = response.data.errmsgs[i];
+                                    var errmsg = response.errmsgs[i];
                                     console.log(errmsg);
                                 }
                             }                
                             this.$Message.info("成功: " + succ_cnt + " / " + (succ_cnt + fail_cnt));
-                        }).catch((error) => {
-                            this.$Message.error(error);
-                            console.log("jsonHttp catch error", error);
-                        })         
+                        });
                     } else {
                         console.log("failed!")
                         this.$Message.error('发布信息有错误，请更正!');
@@ -504,6 +505,34 @@
             showNew() {
                 this.editModalNew = true;
             },
+
+            handleReLogin() {
+                this.reLogin = false;
+                this.$router.push('/login');
+            },
+
+            sendRequest(path, args, fn) {
+                this.$jsonHttp.post(path, args).then((response) => {
+                    if (response.status != 200) {
+                      this.$Message.error("失败: ", response.status, response.statusText)
+                      console.error("失败：%d %s", response.status, response.statusText)
+                      return
+                    }
+                    if (response.data.errmsg !== "") {
+                        if (response.data.errmsg === "need login") {
+                            //需要重新登录
+                            this.reLogin = true;
+                        } else {
+                            this.$Message.error(response.data.errmsg);
+                        }
+                        return
+                    }
+                    fn(response.data)
+                }).catch((error) => {
+                    this.$Message.error(error);
+                    console.log("jsonHttp post catch error", error);                    
+                })
+            }
     	},
         mounted() {
             this.handleList()
