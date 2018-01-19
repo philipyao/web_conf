1657f8bd823beb882dc147721ec9f878ad7dea02
philipyao <187239625@qq.com>
2018-01-19 21:06:20 +0800

    login

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-Vue.use(Router)
+Vue.use(Router);
+
+
+//设置cookie, expiredays为null则有效期为默认session
+Vue.prototype.setCookie = (name, value, expiredays) => {
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + expiredays);　　　　
+  document.cookie = name"=" + escape(value) + ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString());
+};
+
+//获取cookie
+var getCookie = (name) => {
+  var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+  if (arr = document.cookie.match(reg)) {
+    return (arr[2]);
+  } else {
+    return null;
+  }   
+};
+Vue.prototype.getCookie = getCookie;
+
+//删除cookie
+Vue.prototype.delCookie = (name) => {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = getCookie(name);
+    if (cval != null) {
+      document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+    }
+};
 
 const router = new Router({
   routes: [ 
@@ -20,13 +49,12 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
     //获取用户是否登陆
-    let userInfo = window.localStorage.getItem('userinfo')
-    if (!userInfo && to.path != '/login') {
-        next({path: '/login'})
-    } else if (to.path == '/login' && userInfo) {
-        next({path: '/config'})
+    let sessid = getCookie("sessid");
+    if (sessid === null && to.path !== '/login') {
+      next({path: '/login'});
+    } else if (sessid !== null && to.path !== '/login') {
+      next({path: '/config'})
     }
-    console.log("route to ", to.path)
     next()
 })
 
